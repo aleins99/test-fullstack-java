75caa42af802298c9454277c30c90b77cd85f18d
Alejandro Marin <alexword1893@gmail.com>
2023-07-22 16:33:00 -0400

    proyecto creado

---
+++ src/test/java/com/test/fullstack/java/test_java/TestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.fullstack.java.test_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
