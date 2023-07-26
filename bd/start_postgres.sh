3dccd561ceffbd680c10d62848155e25cca17984
Alejandro Marin <alexword1893@gmail.com>
2023-07-26 19:50:41 -0400

    entidad de usuario con metodos post y get realizados

---
+++ bd/start_postgres.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+which psql > /dev/null || (echoerr "Please ensure that postgres client is in your PATH" && exit 1)
+
+mkdir -p $HOME/docker/volumes/postgres
+rm -rf $HOME/docker/volumes/postgres/data
+
+docker run --rm --name pg-docker -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=dev -d -p 5433:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql postgres
+sleep 3
+export PGPASSWORD=postgres
+cat esquemas.sql | docker exec -i pg-docker psql -U postgres -d dev
+cat datos.sql | docker exec -i pg-docker psql -U postgres -d dev
